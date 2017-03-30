d530d3b6902764de11c78add6e156d3b3b87e8ac
JCannon1 <jessecannon1@hotmail.com>
2017-03-30 18:43:57 -0400

    initial commit

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+// connect
+
+// live - azure
+$conn = new PDO('mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=comp1006jessecannondatabase', 'bf3c946f4d66ff', '1d953141');
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
---
+++ validate.php
@@ -0,0 +1,31 @@
+<?php ob_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+require_once ('db.php');
+
+$sql = "SELECT userId, password FROM users WHERE username = :username";
+
+$cmd = $conn->prepare($sql);
+$cmd->bindParam(':username', $username, PDO::PARAM_STR, 50);
+$cmd->execute();
+
+$user = $cmd->fetch();
+
+if (password_verify($password, $user['password'])) {
+    // user found
+    session_start(); // access the existing session
+    $_SESSION['userId'] = $user['userId']; // store the user's id in a session variable
+    $_SESSION['username'] = $username;
+    header('location:albums.php');  // take authenticated user to albums page
+}
+else {
+    // user not found
+    header('location:login.php?invalid=true');
+    exit();
+}
+
+$conn = null;
+
+ob_flush(); ?>
